72e047b9d153bd16e42f0b73d15c4b8be312b518
Renata Cummins <renatacummins@gmail.com>
2019-04-17 11:50:52 -0400

    Add KnightPathFinder::valid_moves with minimal functionality

--- lib/01_knight_path_finder.rb
+++ lib/01_knight_path_finder.rb
@@ -3,6 +3,21 @@ require_relative '00_tree_node'
 class KnightPathFinder
     attr_reader :root_node
 
+    def self.valid_moves(pos)
+        short_offsets = [-1, 1]
+        long_offsets = [-2, 2]
+        moves = []
+
+        long_offsets.each do |long_offset|
+            short_offsets.each do |short_offset|
+                moves << [pos.first + long_offset, pos.last + short_offset]
+                moves << [pos.first + short_offset, pos.last + long_offset]
+            end
+        end
+
+        return moves
+    end
+
     def initialize(pos)
         @root_node = PolyTreeNode.new(pos)
     end
--- spec/01_knight_path_finder_spec.rb
+++ spec/01_knight_path_finder_spec.rb
@@ -4,6 +4,14 @@ require '01_knight_path_finder'
 describe 'KnightPathFinder' do
     let(:kpf) { KnightPathFinder.new([3, 3]) }
 
+    describe '::valid_moves' do
+        it 'returns the correct positions for an initial pos of [3, 3]' do
+            expected_moves = [[1, 2], [2, 1], [1, 4], [4, 1],
+                [5, 2], [2, 5], [5, 4], [4, 5]]
+            expect(KnightPathFinder.valid_moves([3,3])).to eq(expected_moves)
+        end
+    end
+
     describe '#initialize' do
         it 'sets the instance variable @root_node to a PolyTreeNode' do
             expect(kpf.root_node.class).to eq(PolyTreeNode)
